72de022fbff821cc747d86a609f0b3b26f7aff46
Becka <rebecca.robbins.et@gmail.com>
2013-08-05 10:32:28 -0700

    integrated twitterlisten

--- app/models.py
+++ app/models.py
@@ -8,11 +8,56 @@ from sqlalchemy.orm import relationship, backref
 import json
 import datetime
 from app import db 
-import twitterlisten
+import sys
+import tweepy
+import json
+from tweepy.streaming import StreamListener
+from tweepy import Stream 
+from tweepy import OAuthHandler 
+import pygeocoder
+from pygeocoder import Geocoder
+import app 
+from shapely.geometry import Polygon
+
+consumer_key = '8vT4akwvPR1sWQIbO6y8g'
+consumer_secret = '2YQxdvSVPerI51CoewMeIhyKsm8niwKZyuPIQAVg6s'
+access_key= '21369282-5h31qLNIH68WoJIvfoU01DK191ixS8W9g67y41QQ'
+access_secret= 'H2B6Ks97UbPs8XGxRFXNoXQpwO3Os4NTBvNi6b9B0w'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+
+api = tweepy.API(auth)
+#query - opening connecting 
+
 
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
+#not sure what this is....
+
+
+def bs(obj): #to_byte_string
+    if isinstance(obj, str):
+        return obj
+    elif isinstance(obj, unicode):
+        return obj.encode('utf-8')
+    else:
+        return obj
+
+
+for tweet in tweepy.Cursor(api.search, q='san francisco').items(100):
+    #
+    if tweet.coordinates :
+        print bs(tweet.text), bs(tweet.created_at), bs(tweet.coordinates)
+        coordinates = tweet.coordinates['coordinates']
+        #print coordinates
+#       print gmaps.latlng_to_address(coordinates[0], coordinates[1])
+        #store into database
+        print bs("Geocoding: %r, %r" % (coordinates[0],coordinates[1]))
+        results= Geocoder.reverse_geocode(coordinates[1],coordinates[0])
+        print bs("Results: %s" % results)
+        print "\n"
 
 class Neighborhood(db.Model):
 	__tablename__ =  "neighborhoods"
@@ -22,12 +67,14 @@ class Neighborhood(db.Model):
 	#takes a geo tuple and tests to see which neighborhood it fits. 
 
 class Source(db.Model):
+	__tablename__="source"
 	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(64), unique = False)
 	links = db.Column(db.String(128), unique = False)
 
 class Content(db.Model):
 #this gets cleared every Monday
+	__tablename__="content"
 	id = db.Column(db.Integer, primary_key = True)
 	neighborhood_id = db.Column(db.Integer, db.ForeignKey('neighborhood.id'))
 	timestamp = db.Column(db.DateTime)
@@ -35,81 +82,28 @@ class Content(db.Model):
 	hashtags = db.Column(db.String(240))
 	source_id = db.Column(db.Integer, db.ForeignKey('source.id'))
 
-def point_in_neighborhood(x,y,poly): 
+#def point_in_neighborhood(x,y,poly): 
 #test if point in polygon 
     
 
+ 
+Upper_Haight = Neighborhood()
+    #Stanyan and Oak
+Upper_Haight.borders =(Polygon(
+    (37.771034, -122.453849),
+    #Stanyan and Waller:
+    (37.768269, -122.453334),
+    #Divis and Waller
+    (37.770373, -122.436790),
+    #Divis and Oak
+    (37.773154, -122.437391)
+    )
+    )
 
+point = (37.774223, -122.444429)
 
-    #process geodata to determine where it is based on 
-    #define custome method to filter lat long into neighborhood
-
-# import sys
-# import tweepy
-# import json
-# from tweepy.streaming import StreamListener
-# from tweepy import Stream 
-# from tweepy import OAuthHandler 
-# import pygeocoder
-# from pygeocoder import Geocoder
-# import app 
-# #from models import Neighborhood, Source, Content as db_session
-# from shapely.geometry import Polygon
-# #Ratings_24, Ratings_7day, Ratings_month, Ratings_year 
-
-# consumer_key = '8vT4akwvPR1sWQIbO6y8g'
-# consumer_secret = '2YQxdvSVPerI51CoewMeIhyKsm8niwKZyuPIQAVg6s'
-# access_key= '21369282-5h31qLNIH68WoJIvfoU01DK191ixS8W9g67y41QQ'
-# access_secret= 'H2B6Ks97UbPs8XGxRFXNoXQpwO3Os4NTBvNi6b9B0w'
-
-# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-# auth.set_access_token(access_key, access_secret)
-
-# api = tweepy.API(auth)
-# #query - opening connecting 
-
-# def bs(obj): #to_byte_string
-#     if isinstance(obj, str):
-#         return obj
-#     elif isinstance(obj, unicode):
-#         return obj.encode('utf-8')
-#     else:
-#         return obj
-
-
-# for tweet in tweepy.Cursor(api.search, q='san francisco').items(100):
-#     #
-#     if tweet.coordinates :
-#         #import pdb;pdb.set_trace()
-#         print bs(tweet.text), bs(tweet.created_at), bs(tweet.coordinates)
-#         coordinates = tweet.coordinates['coordinates']
-#         #print coordinates
-# #       print gmaps.latlng_to_address(coordinates[0], coordinates[1])
-#         #store into database
-#         print bs("Geocoding: %r, %r" % (coordinates[0],coordinates[1]))
-#         results= Geocoder.reverse_geocode(coordinates[1],coordinates[0])
-#         print bs("Results: %s" % results)
-#         print "\n"
-
-# #define neighborhoods by geo (use pygeocoder)now just doing squares, will upgrade later...
-
-# Upper_Haight = Neighborhood()
-#     #Stanyan and Oak
-# Upper_Haight.borders =(Polygon(
-#     (37.771034, -122.453849),
-#     #Stanyan and Waller:
-#     (37.768269, -122.453334),
-#     #Divis and Waller
-#     (37.770373, -122.436790)
-#     #Divis and Oak
-#     (37.773154, -122.437391)
-#     )
-#     )
-
-# point = (37.774223, -122.444429)
-
-# if point.within(Upper_Haight.borders):
-#     print "yes yes yes"
+if point.within(Upper_Haight.borders):
+    print "yes yes yes"
 
 # if Upper_Haight.point_in_neighborhood(x, y, point
 
