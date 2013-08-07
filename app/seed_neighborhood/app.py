a5eb991f14fcefb269aba18b38a7ffc3a8f03712
Becka <rebecca.robbins.et@gmail.com>
2013-08-07 16:17:50 -0700

    seeding the neighborhoods table. this is a one time use file.

---
+++ app/seed_neighborhood/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+#app.config.from_object("config")
+app.config['DEBUG']=True
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///places.db' 
+# app.config['SQLALCHEMY_DATABASE_URI']='sqlite:////tmp/places.db'
+db = SQLAlchemy(app)
+ 
+# from app import views, models 
