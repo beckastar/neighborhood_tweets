7272cd1db127505de0b214f3e1d3145e22f0dd1e
Becka <rebecca.robbins.et@gmail.com>
2013-08-08 11:25:01 -0700

    trying to match location to neighborhoods

--- app/twitterlisten.py
+++ app/twitterlisten.py
@@ -6,13 +6,14 @@ import json
 from tweepy.streaming import StreamListener
 from tweepy import Stream 
 from tweepy import OAuthHandler 
-import pygeocoder
-from pygeocoder import Geocoder
+# import pygeocoder
+# from pygeocoder import Geocoder
 import app 
 from app import db
 import models
 from models import Neighborhood, Source, Content
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, Point
+# from decimal import Decimal
 #Ratings_24, Ratings_7day, Ratings_month, Ratings_year 
 content = Content()
 neighborhoods = Neighborhood()
@@ -36,34 +37,48 @@ def bs(obj): #to_byte_string
 	else:
 		return obj
 
-
-
 for tweet in tweepy.Cursor(api.search, q='san francisco').items(100):
     if tweet.coordinates :
-        print bs(tweet.text), bs(tweet.created_at), bs(tweet.coordinates)
+        # print bs(tweet.text), bs(tweet.created_at), bs(tweet.coordinates)
+        print "THESE ARE THE COORDINATES", tweet.coordinates['coordinates']
+        print "THIS IS THE LATITUDE", tweet.coordinates['coordinates'][0]
+        print type(tweet.coordinates['coordinates'][0])
         tweets = bs(tweet.text)
         tweetwhen = bs(tweet.created_at)
-
-
-
-
-
         s = tweets.split()
         tags = set()
         for word in s:
         		if word.startswith("#"):
         			tags.add(bs(word))
         			print word
-        tweetwhere= Geocoder.reverse_geocode(coordinates[1],coordinates[0])
-        
+        #tweetwhere= Geocoder.reverse_geocode(coordinates[1],coordinates[0])
+        #determine which neighborhood tweets are tied to
+        # tweet.coordinates = str(tweet.coordinates)
+        # print tweet.coordinates[37:63]
+        #tweet.coordinates = tweet.coordinates[37:63]
+        # print "this is a string", tweet.coordinates
+        # tweet.coordinates = Decimal(tweet.coordinates)
+        # print "this is a float", tweet.coordinates
+        # #print "this is the coordinate", str(tweet.coordinates[37:63])
+        #tweet.coordinates = str(tweet.coordinates)
+        #'-122.16643003, 37.44679093'
+        # point = Point(tweet.coordinates)
+        # neighborhoods=db.session.query(Neighborhood).all()
+        # for neighborhood in neighborhoods:
+        #     if point.within(int(neighborhood.geo)):
+        #         content.id = neighborhood.id
         content.message = tweets
         content.timestamp = tweetwhen
         content.hashtags = " ".join(tags)
         #create instance of content class 
         try:
-        	db.session.add(neighborhoods)
+ #       	db.session.add(neighborhoods)
 	        db.session.add(content)
 	        db.session.commit()
     	except:
     		print "Unicode error."
     		pass
+
+
+
+
