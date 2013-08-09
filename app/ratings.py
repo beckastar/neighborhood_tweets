2b4bdf3a9b6963a112910f5d7bc5152f585e76af
Becka <rebecca.robbins.et@gmail.com>
2013-08-09 16:44:34 -0700

    ratings.py

--- app/ratings.py
+++ app/ratings.py
@@ -7,11 +7,31 @@ from collections import defaultdict
 from sqlalchemy.sql import select
 
  
-query = db.session.query(Content).all()
+query = db.session.query(Content.hashtags).all()
 #rows = db.query.statement.execute().fetchall()
+
 print query
-#d = defaultdict(int)
- 
+
+print query
+
+# d = defaultdict(int)
+l =[]
+for tuples in query:
+	for letter in tuples:
+		print tuples
+		for word in tuples:
+			if word == None:
+				continue
+			elif '#' in word:
+				word = word.split("#")
+				l.append(word)
+			# for letter in word:
+			# 	if letter == None:
+			# 		continue
+			# 	elif letter == "#":
+	 	# 			l.append("#")
+print l
+
 
 # 	print hashes
 # 	d[hashes]+=1
