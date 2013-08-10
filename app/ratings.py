43e676847a7386e0b40043b3604ba871d57c6b0a
Becka <rebecca.robbins.et@gmail.com>
2013-08-09 19:10:34 -0700

    prettified and made dict functional for hashtags

--- app/ratings.py
+++ app/ratings.py
@@ -12,30 +12,46 @@ query = db.session.query(Content.hashtags).all()
 
 print query
 
-print query
-
-# d = defaultdict(int)
-l =[]
+d = defaultdict(int)
 for tuples in query:
-	for letter in tuples:
-		print tuples
-		for word in tuples:
-			if word == None:
-				continue
-			elif '#' in word:
-				word = word.split("#")
-				l.append(word)
+	# print tuples
+	for s in tuples:
+		# s is a string, e..g "#hashtag#another"
+		if not s:
+			continue
+		words = s.split("#")
+		words = words[1:]
+		for word in words:
+			d[word] += 1
+import pprint
+pprint.pprint (dict(d))
+
+			# print "THESE ARE WORDS", words
+			# print len(words)
+			# for item in words:
+			# 	item = str(item)
+			# 	if item not in d.values():
+			# 		d[item] = 1
+			# 	else:
+			# 		d[item] +=1
+			# # d[s] += len(words)
+			# elif '#' in word:
+			# 	l.append(word)
 			# for letter in word:
 			# 	if letter == None:
 			# 		continue
 			# 	elif letter == "#":
-	 	# 			l.append("#")
-print l
+			# 		word = word.split("#")
+			# 		word = str(word)
+		 # 				l.append("#")
+		 # 				d[word]+=1
+# print l
+# print "THIS IS YOUR DICTIONARY",d
 
 
-# 	print hashes
-# 	d[hashes]+=1
 
+# for word in l:
+# 	
 # print d 
 
 
