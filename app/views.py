39fce02515256982c331cdf9e540bcae8e8e1c12
Becka <rebecca.robbins.et@gmail.com>
2013-08-11 13:37:49 -0700

    sample google chart that works in index.html

--- app/views.py
+++ app/views.py
@@ -1,96 +1,55 @@
-from flask import render_template, flash, redirect, session, url_for, request, g
-from flask.ext.login import login_user, logout_user, current_user, login_required
-from app import app, db
-#from forms import LoginForm, EditForm
-#from models import User, ROLE_USER, ROLE_ADMIN
+from flask import render_template, flash, redirect, session, url_for 
+from app import app, db 
 from datetime import datetime
+from flask import render_template
+from models import Neighborhood, Content
 
-#On front end:
-#-- What am I showing? Linking visualization to databases. No need to worry right now about 
-#-- ... what? What do i put here? 
-# ... link to twitter.txt
-#
+ 
 
+# @app.before_request
+# def before_request(): 
+# @app.errorhandler(404)
+# def internal_error(error):
+#     return render_template('404.html'), 404
 
-# @lm.user_loader
-# def load_user(id):
-#     return User.query.get(int(id))
-
-@app.before_request
-def before_request():
-    g.tweet = current_user
-    #g.user = current_user
-    if g.user.is_authenticated():
-        g.user.last_seen = datetime.utcnow()
-        db.session.add(g.user)
-        db.session.commit()
-
-@app.errorhandler(404)
-def internal_error(error):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_error(error):
-    db.session.rollback()
-    return render_template('500.html'), 500
+# @app.errorhandler(500)
+# def internal_error(error):
+#     db.session.rollback()
+#     return render_template('500.html'), 500
 
 @app.route('/')
-@app.route('/index')
-def index():
-    user = g.user
-    # posts = [
-    #     { 
-    #         'author': { 'nickname': 'John' }, 
-    #         'body': 'Beautiful day in Portland!' 
-    #     },
-    #     { 
-    #         'author': { 'nickname': 'Susan' }, 
-    #         'body': 'The Avengers movie was so cool!' 
-    #     }
-    # ]
-    return render_template('index.html',
-        title = 'Home',
-        user = user,
-        posts = posts)
+def index(): 
+        #return render_template("index.html")
+		data = [('Western Addition', 11), ('Upper Haight', 30), ('Potrero', 15), ('Dogpatch', 42),
+			('Pacific Heights', 38), ('Fillmore', 45), ('Cole Valley', 52)]
+		return render_template('index.html', data=data)
+       
 
-#@app.route('/login', methods = ['GET', 'POST'])
-# @oid.loginhandler
-# def login():
-#     if g.user is not None and g.user.is_authenticated():
-#         return redirect(url_for('index'))
-#     form = LoginForm()
-#     if form.validate_on_submit():
-#         session['remember_me'] = form.remember_me.data
-#         return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
-#     return render_template('login.html', 
-#         title = 'Sign In',
-#         form = form,
-#         providers = app.config['OPENID_PROVIDERS'])
+@app.route('/chart/') 
+def hello():
+	return 
+
+@app.route('/layout/')
+def layouts():
+	return render_template()
+
+     
+
+# @app.route('/twitterfeed')
+# def twitterfeed():
+# 	return render_template("twitterfeed.html", 
+# 		title = "test")
 
 
-@app.route('/edit', methods = ['GET', 'POST']) 
-def edit():
-    form = EditForm(g.user.nickname)
-    if form.validate_on_submit():
-        g.user.nickname = form.nickname.data
-        g.user.about_me = form.about_me.data
-        db.session.add(g.user)
-        db.session.commit()
-        flash('Your changes have been saved.')
-        return redirect(url_for('edit'))
-    elif request.method != "POST":
-        form.nickname.data = g.user.nickname
-        form.about_me.data = g.user.about_me
-    return render_template('edit.html',
-        form = form)
-
-@app.route('/twitterfeed')
-def twitterfeed():
-	return render_template("twitterfeed.html")
-
-
-@app.route("/my_map")
-def my_map():
-	print "This is a map"
-	return render_template("map.html")
+# @app.route("/my_map")
+# def my_map():
+# 	print "This is a map"
+# 	return render_template("map.html")
  
+#  if __name__ == '__main__':
+# 	# Automatically detect changes to charts.py and reload the server as
+# 	# necessary.
+# 	app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run()
