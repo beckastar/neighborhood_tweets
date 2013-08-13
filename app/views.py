4d89c2dcd69bfdc13330b6529dbcd789b751d1ef
Becka <rebecca.robbins.et@gmail.com>
2013-08-13 12:24:30 -0700

    got keys in dictionary to work

--- app/views.py
+++ app/views.py
@@ -6,21 +6,11 @@ from models import Neighborhood, Content
 from flask import jsonify
  
 
-# @app.before_request
-# def before_request(): 
-# @app.errorhandler(404)
-# def internal_error(error):
-#     return render_template('404.html'), 404
-
-# @app.errorhandler(500)
-# def internal_error(error):
-#     db.session.rollback()
-#     return render_template('500.html'), 500
-
 @app.route("/fish")
 def results_json():
 	cols = ['neighborhood_id']
 	data = Content.query.filter(Content.neighborhood_id != None).all()
+
 	counts = {}
 	for c in data:
 		if counts.get(c.neighborhood_id):
@@ -28,33 +18,36 @@ def results_json():
 		else:
 			counts[c.neighborhood_id] = 1
 
-	# result = [{col:getattr(d, col) for col in cols} for d in data]
 	c = jsonify(counts)
-	# l = []
-	# for k, v in c:
-	# 	l.append[(k, d[k])] 
+	print c
+	#link key in c to neighborhood.name
 	return c
+
+@app.route("/testing")
+def make_list():
+	#data = Content.query(Content.neighborhood_id).all()
+	data = Neighborhood.query.filter(Neighborhood.name != None).all()
+	data_num = Content.query.filter(Content.neighborhood_id != None).all()
+	data_dict ={} 
+	for d in data:
+		if data_dict.get(d.name):
+			data_dict[d.name] = 1
+		else:
+			data_dict[d.name]=0
+			#this above line here just generated all of the keys.		
+	j = jsonify(data_dict)
+	return j
+	#this works now! put data in it. 
+	# hoods = {}
+	# hoods(zip(data,data_name))
+	# return hoods
+	# look up query examples
+
  
-
-#next step: get google charts to take in data. look up jinja 
-
 @app.route('/')
 @app.route('/charts')
 def hello():
 	return render_template('charts.html')
-
-
-# @app.route('/layout')
-# def layouts():
-# 	return render_template()
-
-     
- 
- 
-#  if __name__ == '__main__':
-# 	# Automatically detect changes to charts.py and reload the server as
-# 	# necessary.
-# 	app.run(debug=True)
 
 if __name__ == "__main__":
     app.run()
