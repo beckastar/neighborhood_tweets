8b4d827713d0dc01b0f57d71beed5a3bafb6898f
Becka <rebecca.robbins.et@gmail.com>
2013-08-15 10:51:39 -0700

    have created list of timestamps for given period.

--- app/seed_ratings_24.py
+++ app/seed_ratings_24.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, types 
 from app import db
 from models import Neighborhood, Source, Content
-from datetime import datetime
+from datetime import datetime, timedelta
 import time 
 import pytz
 from pytz import all_timezones
@@ -10,20 +10,67 @@ from pytz import all_timezones
 utc = pytz.timezone("UTC")
 pacific = pytz.timezone("US/Pacific-New")
 
-#change datetime.now to utc ---- DONE
-# dt = datetime.now()
-# print dt
-# dtutc = datetime.utcnow()
-# print dtutc
+l = []
+date1 = datetime.now()
+print date1
+#24 hours earlier
+date2 = date1-timedelta(7)
+print date2
+
+
+
+data = Content.query.filter(Content.timestamp != None).all()
+
+
+dates = [] 
+for d in data:
+	d.timestamp =  datetime.strptime(str(d.timestamp), "%Y-%m-%d %H:%M:%S")
+	if d.timestamp < date1 and d.timestamp > date2:
+		dates.append(d.timestamp)
+
+print dates
+
+
+
+
+
+# date2=
+
+# for d in l:
+# 	if d < date1 and d> date2:
+# 		l.append[d]
+
+
+#trouble getting ratings out of table. 
+# for d in data:
+# 	d = d.split()
+# 	print d
+
+
+# date_today = datetime.today()
+# print date_today
+ 
+	# formatted_timestamp =  datetime.strptime(str(timestamp), "%Y-%m-%d %H:%M:%S")
+	# if formatted_timestamp == date_today:
+	# 	print "yes"
+
+# 	if formatted_timestamp <_________________ and formatted_timestamp > ____________________:
+
+
+ 
+
+# #standardize time format:
+# timestamp = '2013-08-14 18:04:56'
+# thing =  datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
+# print thing
+
+
+
+
 
 #if tweets are in the range of midnight to midnight san francisco time on yesterday's date
 #
 #find out if tweets are in a range 
-
-#probably need to standardize format 
-#sample timestamp:2013-08-14 18:04:56
-#YYYY-MM-DD HH:MM:SS 
-#this is just a string. use strptime() to turn it into a datetime object
 
 #timestamp = 2013-08-14 18:04:56
 #dt_object = datetime.strptime(timestamp, %Y-%M-%D %H:%M
@@ -37,15 +84,7 @@ pacific = pytz.timezone("US/Pacific-New")
 # twitter in greenwich mean time 
 
 #<, >, = are all valid arguments 
-
-time_data = 
-
-timestamp = '2013-08-14 18:04:56'
-thing =  datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S")
-print thing
-
-for c in 
-
+  
 
 
 # for item in content_timestamp:
